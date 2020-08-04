230cf52a821d907730738defefa61b23b56f658a
Miska Kytö <miska.kyto@gmail.com>
2020-08-04 16:15:17 +0300

    updated url

--- src/actions/bot.js
+++ src/actions/bot.js
@@ -4,7 +4,7 @@ import INFO from 'constants'
 
 export const changeAccess = (channelId, bool) => (dispatch, getState) => {
     const { token } = getState().main
-    return axios.post(`${INFO.baseUrl}/api/`, {
+    return axios.post(`${INFO.backendUrl}/api/`, {
         channelId,
         bool,
         token
